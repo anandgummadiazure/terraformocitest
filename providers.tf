4ca7089a15d273507e92431aedd10f5e055d113d
anandgummadiazure <94324617+anandgummadiazure@users.noreply.github.com>
2022-07-07 10:02:33 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,20 +1,16 @@
-## Copyright (c) 2021, Oracle and/or its affiliates.
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-
-provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  region       = var.region
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "4.82.0"
+    }
+  }
 }
 
 provider "oci" {
-  alias        = "home_region"
-  tenancy_ocid = var.tenancy_ocid
-  region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
-}
-
-provider "oci" {
-  alias        = "current_region"
-  tenancy_ocid = var.tenancy_ocid
-  region       = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
+  region           = var.region
 }
